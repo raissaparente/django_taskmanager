a1e42fa7ba152b44d37725535237e0f80f0151ce
RaissaParente <raissabparente@gmail.com>
2024-04-27 14:34:34 -0300

    adicionei categorias

--- base/models.py
+++ base/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
--- base/views.py
+++ base/views.py
@@ -3,11 +3,18 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from .models import Task
+from .models import Task, Category
 
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.request.GET.get('category')
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
 
     def get_context_data(self,  **kwargs):
         context = super().get_context_data(**kwargs)
@@ -16,6 +23,7 @@ class TaskList(ListView):
             context['tasks'] = context['tasks'].filter(title__icontains=search_input)
 
         context['search_input'] = search_input
+        context['categories'] = Category.objects.all()
 
         return context
 
